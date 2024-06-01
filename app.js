33d550af39460da311e34bb3cc607d6875f8f9d0
zuzannabrauer <entropia.b137@gmail.com>
2024-06-01 19:37:00 +0000

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 var booksRouter = require('./routes/books');
 
@@ -11,7 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors())
 app.use('/books', booksRouter);
 
 app.use(function(req, res, next) {
