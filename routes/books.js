057845d0bed9ff0bd770735f77d40f259079657d
zuzannabrauer <entropia.b137@gmail.com>
2024-06-13 19:52:22 +0000

    Working favourites and searchbar

--- routes/books.js
+++ routes/books.js
@@ -25,29 +25,31 @@ router.get('/', (req, res) => {
       res.status(400).json({"error":err.message})
       return
     }
+    console.log(rows)
     res.json(rows.map(row => ({
       id: row.ID,
       title: row.title,
       author: row.author,
       pages: row.pages,
       year: row.year,
-      genre: row.genre
+      genre: row.genre,
+      isInFavourites: row.in_favourites
     })))
   })
 })
 
 router.post('/', upload.single('file'), (req, res) => {
-  const sql = 'insert into book (title, author, pages, year, genre) values (?, ?, ?, ?, ?)'
+  const sql = 'insert into book (title, author, pages, year, genre, in_favourites) values (?, ?, ?, ?, ?, ?)'
   console.log(req.file)
   console.log(req.body)
   const { title, author, pages, year, genre } = req.body
-  const params = [title, author, pages, year, genre]
+  const params = [title, author, pages, year, genre, false]
   db.run(sql, params, function (err, result) {  // do NOT change to arrow function
     console.log(err)
     console.log(result)
     fs.rename(req.file.path, `./uploads/${this.lastID}`, console.log)
     res.status(201).json({
-      id: this.lastID, title, author, pages, year, genre
+      id: this.lastID, title, author, pages, year, genre, isInFavourites: false
     })
   })
 })
@@ -75,6 +77,15 @@ router.delete('/:id', (req, res) => {
   db.run(sql, params, (err, result) => {
     fs.unlink(`./uploads/${req.params.id}`, console.log)
     res.status(204).send()
+  })
+})
+
+router.put('/:id/favourites/:isInFavourites', (req, res) => {
+  const sql = 'update book set in_favourites = ? WHERE ID = ?'
+  console.log(req.params)
+  const params = [req.params.isInFavourites === 'true' ? 1 : 0, req.params.id]
+  db.run(sql, params, (err, result) => {
+    res.json({ id: req.params.id, isInFavourites: req.params.isInFavourites === 'true' })
   })
 })
 
